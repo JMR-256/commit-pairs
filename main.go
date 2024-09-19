7e83be86c0419c2f52668188857c0889b7f8e05a
James Riley <james.riley@sky.uk>
2024-09-19 14:00:38 +0100

    fix: Changed the flag parsing to check if message exists in whole list
      - This allows user to pass a list of initials before -m flag

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -18,37 +19,66 @@ const DaysPairFile = ".daysPair"
 var homeDirectory string
 
 func main() {
-	args := os.Args[1:]
 	homeDirectory = resolveHomeDirectory()
+
+	messageFlag := flag.String("m", "", "Commit message to include inline")
+	pairsFlag := flag.Bool("p", false, "Provide a list of contributor initials")
+	helpFlag := flag.Bool("h", false, "Display help message")
+
+	flag.Parse()
+	args := flag.Args()
+
+	if *helpFlag {
+		fmt.Println("Usage:")
+		fmt.Println("  git pc [-m 'inline message'] [list of initials e.g. JD AS TJ]")
+		fmt.Println("  git pc [-p] [list of initials]")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	// Handle the -p flag
+	if *pairsFlag {
+		if len(flag.Args()) == 0 {
+			log.Fatalf("Error: No contributor initials provided after -p")
+		}
+		writePairsToFile(flag.Args())
+		os.Exit(0)
+	}
 
 	var commitMessage string
 	var contributorInitials []string
 
-	if len(args) == 0 {
-		contributorInitials = loadContributorsFromFile()
+	// Handle the -m flag for commit message
+	if *messageFlag != "" {
+		commitMessage = *messageFlag
 	} else {
-		switch args[0] {
-		case "-m", "--message":
-			if len(args) < 2 {
-				log.Fatalf("Error: No commit message provided after %v", args[0])
+		// If no message flag, check if the arguments contain "-m"
+		for i, arg := range args {
+			if arg == "-m" {
+				// If "-m" is found, the next argument should be the commit message
+				if i+1 < len(args) {
+					commitMessage = args[i+1]
+				} else {
+					log.Fatal("Error: No commit message provided after -m")
+				}
+				// The initials are the arguments before "-m"
+				contributorInitials = args[:i]
+				break
 			}
-			commitMessage = args[1]
-			if len(args[2:]) > 0 {
-				contributorInitials = args[2:]
-			} else {
-				contributorInitials = loadContributorsFromFile()
-			}
-
-		case "-p", "--pairs":
-			writePairsToFile(args[1:])
-			os.Exit(0)
-
-		default:
-			contributorInitials = args
 		}
 	}
 
-	coAuthors := resolveCommitDetails(contributorInitials)
+	// If "-m" wasn't found in the args, treat all args as contributor initials
+	if commitMessage == "" {
+		contributorInitials = args
+	}
+
+	// If no contributors specified - read from file
+	if len(contributorInitials) == 0 {
+		contributorInitials = loadContributorsFromFile()
+	}
+
+	coAuthors := resolveContributorDetails(contributorInitials)
 	commit(commitMessage, coAuthors)
 }
 
@@ -64,7 +94,7 @@ func writePairsToFile(initials []string) {
 	writeFile(DaysPairFile, strings.Join(initials, " "))
 }
 
-func resolveCommitDetails(contributorInitials []string) (coAuthors string) {
+func resolveContributorDetails(contributorInitials []string) (coAuthors string) {
 	contributors, domain := parsePairsFile()
 
 	primary := contributors[contributorInitials[0]]
@@ -83,7 +113,16 @@ func commit(commitMessage string, coAuthors string) {
 	if commitMessage == "" {
 		// Write co authors to commit template file to be read with -t flag on commit
 		// We do this so that authors appear automatically in native text editor when not providing message
+		// Using --edit -m instead will not abort an unsaved commit message so this is the alternative
 		writeFile(CommitTemplateFile, coAuthors)
+
+		// Ensure that the file is deleted after the function completes
+		defer func() {
+			err := os.Remove(filepath.Join(homeDirectory, CommitTemplateFile))
+			if err != nil {
+				log.Printf("Error deleting file: %v", err)
+			}
+		}()
 		executeCommitWithTemplate()
 	} else {
 		executeCommitWithoutTemplate(commitMessage, coAuthors)
@@ -211,7 +250,7 @@ func resolveCoAuthorDetails(contributorInitials []string, contributors map[strin
 	}
 
 	var formattedCoAuthors strings.Builder
-
+	formattedCoAuthors.WriteString("\n")
 	for _, value := range coAuthorDetails {
 		line := "Co-authored-by: " + value[0] + " <" + value[1] + "@" + domain + ">\n"
 		formattedCoAuthors.WriteString(line)
